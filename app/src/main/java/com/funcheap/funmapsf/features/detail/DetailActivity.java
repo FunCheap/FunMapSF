d3e6b03da8835c801e888e8baed829cff930a893
Ganesh S Kudva <ganehskudva@gmail.com>
2017-10-15 16:41:43 -0700

    Detail Activity changes

--- app/src/main/java/com/funcheap/funmapsf/features/detail/DetailActivity.java
+++ app/src/main/java/com/funcheap/funmapsf/features/detail/DetailActivity.java
@@ -1,12 +1,132 @@
 package com.funcheap.funmapsf.features.detail;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * Created by Jayson on 10/11/2017.
- *
- * Activity to show event details.
- */
+import com.funcheap.funmapsf.R;
+import com.funcheap.funmapsf.commons.models.Events;
+import com.funcheap.funmapsf.databinding.ActivityDetailBinding;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
-public class DetailActivity extends Activity {
+import java.util.Calendar;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsing_toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.ivBackdrop)
+    ImageView ivBackdrop;
+
+    private ActivityDetailBinding mBinding;
+    private Events mEvents;
+    private static final String EVENT_EXTRA = "event_extra";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_detail);
+        ButterKnife.bind(this);
+
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mEvents = getIntent().getExtras().getParcelable(EVENT_EXTRA);
+        if (mEvents != null)
+        {
+            mBinding.setEvents(mEvents);
+        }
+    }
+
+    public void onCalenderClick(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onCalenderClick", Toast.LENGTH_SHORT).show();
+        showDate();
+    }
+
+    public void onDirectionsClick(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onDirectionsClick", Toast.LENGTH_SHORT).show();
+        String uri = "http://maps.google.com/maps?daddr=" + "37.8199 N,122.4483 W";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
+    }
+
+    public void onShareClick(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onShareClick", Toast.LENGTH_SHORT).show();
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mEvents.getTitle());
+        startActivity(Intent.createChooser(shareIntent, "Share link using"));
+    }
+
+    public void onSaveClick(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onSaveClick", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
+     //   setTime(hour, minute);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+     //   setDate(year, month, day);
+        showTime();
+    }
+
+    public void showDate()
+    {
+        Date date = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(DetailActivity.this, year, month, day);
+
+        datePickerDialog.show(getFragmentManager(), "DateFragment");
+    }
+
+    public void showTime()
+    {
+        Date date = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(DetailActivity.this, hour, minute, DateFormat.is24HourFormat(getApplicationContext()));
+
+        timePickerDialog.show(getFragmentManager(), "TimeFragment");
+    }
 }
