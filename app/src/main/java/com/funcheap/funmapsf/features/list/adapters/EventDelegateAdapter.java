5bc0e81a943c03e82deee68e7bbf96a48484e05e
Ganesh S Kudva <ganehskudva@gmail.com>
2017-10-22 17:26:24 -0700

    Week-2 Detail Activity changes

--- app/src/main/java/com/funcheap/funmapsf/features/list/adapters/EventDelegateAdapter.java
+++ app/src/main/java/com/funcheap/funmapsf/features/list/adapters/EventDelegateAdapter.java
@@ -3,22 +3,28 @@ package com.funcheap.funmapsf.features.list.adapters;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.funcheap.funmapsf.R;
 import com.funcheap.funmapsf.commons.models.Events;
 import com.funcheap.funmapsf.features.detail.DetailActivity;
 import com.hannesdorfmann.adapterdelegates3.AdapterDelegate;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.funcheap.funmapsf.R.id.ivBackdrop;
+import static com.funcheap.funmapsf.R.id.ivItemImg;
 
 /**
  * Created by Jayson on 10/13/2017.
@@ -49,15 +55,6 @@ public class EventDelegateAdapter extends AdapterDelegate<List<Events>> {
     @Override
     protected RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent) {
         View view = mInflater.inflate(R.layout.listitem_event, parent, false);
-        view.setOnClickListener( myView -> {
-            if (mEvent != null)
-            {
-                Intent intent = new Intent(mContext, DetailActivity.class);
-                intent.putExtra(EVENT_EXTRA, mEvent);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.startActivity(intent);
-            }
-        });
         return new EventViewHolder(view);
     }
 
@@ -75,6 +72,28 @@ public class EventDelegateAdapter extends AdapterDelegate<List<Events>> {
         viewHolder.dateTime.setText(items.get(position).getStartDate());
         viewHolder.price.setText(items.get(position).getCost());
         viewHolder.venue.setText(items.get(position).getVenue().getVenueAddress());
+
+        Picasso.Builder builder = new Picasso.Builder(mContext);
+        builder.indicatorsEnabled(true);
+        builder.listener(new Picasso.Listener()
+        {
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception)
+            {
+                exception.printStackTrace();
+            }
+        });
+        builder.build().load(mEvent.getThumbnail()).fit().centerCrop().into(viewHolder.ivItemImg);
+
+        holder.itemView.setOnClickListener(myView -> {
+            if (mEvent != null)
+            {
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra(EVENT_EXTRA, items.get(position));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     /**
@@ -90,6 +109,8 @@ public class EventDelegateAdapter extends AdapterDelegate<List<Events>> {
         public TextView price;
         @BindView(R.id.text_venue)
         public TextView venue;
+        @BindView(R.id.ivItemImg)
+        public ImageView ivItemImg;
 
         public EventViewHolder(View itemView) {
             super(itemView);
