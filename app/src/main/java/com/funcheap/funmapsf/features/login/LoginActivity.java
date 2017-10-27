8bf518602593314d4167531715c6ae91ba931065
Anushree Ganjam <anushreeaganjam@gmail.com>
2017-10-27 16:54:15 -0700

    Adding the Splash screen and launcher icon for the app

--- app/src/main/java/com/funcheap/funmapsf/features/login/LoginActivity.java
+++ app/src/main/java/com/funcheap/funmapsf/features/login/LoginActivity.java
@@ -1,18 +1,18 @@
 package com.funcheap.funmapsf.features.login;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.funcheap.funmapsf.R;
 import com.funcheap.funmapsf.features.home.HomeActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static com.funcheap.funmapsf.R.id.btnFB;
 
 /**
  * A login screen that offers login via email/password.
@@ -27,6 +27,9 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.btnGuestLogin)
     Button btnGuest;
 
+    @BindView(R.id.splash_layout)
+    LinearLayout splash_screen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +39,43 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    public void onLogin(View view)
-    {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+    public void onLogin(View view) {
+        btnGuest.setVisibility(View.GONE);
+        btnGoogle.setVisibility(View.GONE);
+        btnFB.setVisibility(View.GONE);
+        DisplaySplashTask task = new DisplaySplashTask();
+        task.execute();
+
     }
 
+    private class DisplaySplashTask extends AsyncTask<Void, Void, Void> {
 
+        @Override
+        protected void onPreExecute() {
+            splash_screen.setVisibility(View.VISIBLE);
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //some heavy processing resulting in a Data String
+            for (int i = 0; i < 2; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+
+        }
+    }
 }
 
