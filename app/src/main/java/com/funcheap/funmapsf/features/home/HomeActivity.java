6fde5290af45890bf4c93e5dbcdffde23f4d1699
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-10-27 11:46:52 -0700

    Replaced design support bottomnav with a different library.

--- app/src/main/java/com/funcheap/funmapsf/features/home/HomeActivity.java
+++ app/src/main/java/com/funcheap/funmapsf/features/home/HomeActivity.java
@@ -3,8 +3,8 @@ package com.funcheap.funmapsf.features.home;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +24,7 @@ import com.funcheap.funmapsf.features.map.MapsViewModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
 /**
  * Created by Jayson on 10/12/2017.
@@ -44,7 +45,7 @@ public class HomeActivity extends AppCompatActivity implements
     private EditFilterDiaglogFragment mSelectedFragment;
 
     @BindView(R.id.bottom_navigation)
-    public BottomNavigationView mBottomNav;
+    public BottomNavigation mBottomNav;
 
     public MapsViewModel mMapsModel;
     public ListFilterViewModel mListFiltersViewModel;
@@ -90,7 +91,7 @@ public class HomeActivity extends AppCompatActivity implements
     private void loadFragments() {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.disallowAddToBackStack()
-                .add(R.id.content_frame_home, mHomeFragment, mHomeFragment.getClass().getSimpleName())
+                .add(R.id.content_frame_home, mHomeFragment, TAG_MAP_FRAGMENT)
                 .commit();
     }
 
@@ -98,52 +99,58 @@ public class HomeActivity extends AppCompatActivity implements
         /*
          * Programmatically show and hide fragments based on the selection
          */
-        mBottomNav.setOnNavigationItemSelectedListener(item -> {
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            switch (item.getItemId()) {
-                case R.id.action_search:
-                    ft.disallowAddToBackStack();
-                    ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-                    if (fm.findFragmentByTag(TAG_FILTERS_FRAGMENT) != null) {
-                        ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_FILTERS_FRAGMENT));
+        mBottomNav.setOnMenuItemClickListener(
+                new BottomNavigation.OnMenuItemSelectionListener() {
+                    @Override
+                    public void onMenuItemSelect(@IdRes int item, int i1, boolean b) {
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        switch (item) {
+                            case R.id.action_search:
+                                ft.disallowAddToBackStack();
+                                ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                                if (fm.findFragmentByTag(TAG_FILTERS_FRAGMENT) != null) {
+                                    ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_FILTERS_FRAGMENT));
+                                }
+                                if (fm.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) != null) {
+                                    ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_BOOKMARKS_FRAGMENT));
+                                }
+                                ft.show(mHomeFragment)
+                                        .commit();
+                                break;
+                            case R.id.action_filters:
+                                ft.disallowAddToBackStack()
+                                        .hide(mHomeFragment);
+                                ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                                if (fm.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) != null) {
+                                    ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_BOOKMARKS_FRAGMENT));
+                                }
+                                ft.add(R.id.content_frame_home, ListFiltersFragment.newInstance(), TAG_FILTERS_FRAGMENT)
+                                        .commit();
+                                break;
+                            case R.id.action_bookmarks:
+                                ft.disallowAddToBackStack()
+                                        .hide(mHomeFragment);
+                                ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                                if (fm.findFragmentByTag(TAG_FILTERS_FRAGMENT) != null) {
+                                    ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_FILTERS_FRAGMENT));
+                                }
+                                ft.add(R.id.content_frame_home, ListBookmarksFragment.newInstance(), TAG_BOOKMARKS_FRAGMENT)
+                                        .commit();
+                                break;
+                            default:
+                                Log.d(TAG, "initBottomNav: Unrecognized menu selection!");
+                                ft.commit();
+                        }
                     }
-                    if (fm.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) != null) {
-                        ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_BOOKMARKS_FRAGMENT));
-                    }
-                    ft.show(mHomeFragment)
-                            .commit();
-                    return true;
-                case R.id.action_filters:
-                    ft.disallowAddToBackStack()
-                            .hide(mHomeFragment);
-                    ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-                    if (fm.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) != null) {
-                        ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_BOOKMARKS_FRAGMENT));
-                    }
-                    ft.add(R.id.content_frame_home, ListFiltersFragment.newInstance(), TAG_FILTERS_FRAGMENT)
-                            .commit();
-                    return true;
-                case R.id.action_bookmarks:
-                    ft.disallowAddToBackStack()
-                            .hide(mHomeFragment);
-                    ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-                    if (fm.findFragmentByTag(TAG_FILTERS_FRAGMENT) != null) {
-                        ft.remove(getSupportFragmentManager().findFragmentByTag(TAG_FILTERS_FRAGMENT));
-                    }
-                    ft.add(R.id.content_frame_home, ListBookmarksFragment.newInstance(), TAG_BOOKMARKS_FRAGMENT)
-                            .commit();
-                    return true;
-                default:
-                    Log.d(TAG, "initBottomNav: Unrecognized menu selection!");
-                    ft.commit();
-                    return false;
-            }
-        });
 
-        // Select the mEditSearch button by default
-        mBottomNav.setSelectedItemId(R.id.action_search);
+                    @Override
+                    public void onMenuItemReselect(@IdRes int id, int i1, boolean b) {
+                        // Do nothing
+                    }
+                });
     }
+
 
     @Override
     public void onBackPressed() {
@@ -174,6 +181,6 @@ public class HomeActivity extends AppCompatActivity implements
      */
     public void setFilter(Filter filter) {
         mMapsModel.setFilter(filter);
-        mBottomNav.setSelectedItemId(R.id.action_search);
+        mBottomNav.setSelectedIndex(0, true);
     }
 }
