22c8f5453660140f88635a1e7190203e4ddeb5cc
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-11-14 12:12:01 -0800

    Toolbar icons are now white to match the theme.

--- app/src/main/java/com/funcheap/funmapsf/features/home/HomeActivity.java
+++ app/src/main/java/com/funcheap/funmapsf/features/home/HomeActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
@@ -11,9 +13,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -71,15 +70,12 @@ public class HomeActivity extends AppCompatActivity {
         initSearchMode();
     }
 
-    private void setIconInMenu(Menu menu, int menuItemId, int labelId, int iconId){
-        MenuItem item = menu.findItem(menuItemId);
-        SpannableStringBuilder builder = new SpannableStringBuilder("    "+this.getResources().getString(labelId));
-        builder.setSpan(new ImageSpan(this,iconId),0,1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        item.setTitle(builder);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
         getMenuInflater().inflate(R.menu.home_toolbar, menu);
+
         if (mMapViewModel.getDisplayMode().getValue() != null) {
             int displayMode = mMapViewModel.getDisplayMode().getValue();
             MenuItem item = mToolbar.getMenu().findItem(R.id.action_filters);
@@ -100,6 +96,16 @@ public class HomeActivity extends AppCompatActivity {
                 item.setIcon(R.drawable.ic_format_list_bulleted);
             }
         }
+
+        // Color icons
+        for(int i = 0; i < menu.size(); i++){
+            Drawable drawable = menu.getItem(i).getIcon();
+            if(drawable != null) {
+                drawable.mutate();
+                drawable.setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+
         return true;
     }
 
